985c4f6efee00b23000a0d2e8cd7be3ce371c644
mre <m.reid@mas-software.de>
2023-12-21 07:00:57 +0100

    nearly working

--- src/simulation/create_payloads.rs
+++ src/simulation/create_payloads.rs
@@ -6,17 +6,37 @@ use std::cell::RefCell;
 use crate::AppState;
 use std::error::Error;
 use crate::Rc;
-pub fn create_payloads(app_state: &Rc<RefCell<AppState>>) -> Result<(), Box<dyn Error>> {
+use rand::Rng;
+pub fn create_payloads(app_state: &Rc<RefCell<AppState>>) -> Result<Vec<String>, Box<dyn Error>> {
     let app_state_borrow = app_state.borrow();
     let dbirth_file_path = app_state_borrow.dbirth_file_path();
     let ddata_file_path = app_state_borrow.ddata_file_path();
     let ddeath_file_path = app_state_borrow.ddeath_file_path();
-println!("#######i am the path {}", dbirth_file_path);
-createDbirthMessages(dbirth_file_path.to_string());
-    Ok(())
+
+    
+let mut payloads: Vec<String> = Vec::new();
+
+    let dbirthMessagesToPush = createDbirthMessages(dbirth_file_path.to_string());
+if let Ok(messages) = dbirthMessagesToPush {
+    payloads.extend(messages);
+} else {
+    println!("didnt work");
+}
+    //TODO: Here i am setting it manually but should be a user entry 
+    let numMessagesToSend: u32 = 30;
+    let ddataMessagesToPush = createDDataMessages(ddata_file_path.to_string(), numMessagesToSend);
+
+if let Ok(messages) = ddataMessagesToPush {
+    payloads.extend(messages);
+} else {
+    println!("didnt work");
 }
 
-fn createDbirthMessages(dbirth_file_path: String) -> Result<(), Box<dyn std::error::Error>> {
+    // println!("payloads: {:?}", payloads);
+    Ok(payloads)
+}
+
+fn createDbirthMessages(dbirth_file_path: String) -> Result<Vec<String>, Box<dyn std::error::Error>> {
     // Open the file
     let file = File::open(dbirth_file_path)?;
     let mut reader = BufReader::new(file);
@@ -25,10 +45,75 @@ fn createDbirthMessages(dbirth_file_path: String) -> Result<(), Box<dyn std::err
     reader.read_to_string(&mut contents)?;
 
     // pass the json
-    let json: Value = serde_json::from_str(&contents)?;
+    let jsonFromFile: Value = serde_json::from_str(&contents)?;
 
+    let metrics = &jsonFromFile["metrics"];
+
+    let mut serialized_metrics = Vec::new();
+    if let serde_json::Value::Array(metrics) = metrics {
+        for metric in metrics {
+          let serialized_metric = serde_json::to_string(metric).unwrap_or_else(|_| "Invalid JSON".to_string());
+          serialized_metrics.push(serialized_metric);
+        }
+    }
     // print the contents for now
-    println!("I am the json file contents: {:?}", json );
+    // println!("I am the json file contents: {:?}", metrics );
 
-    Ok(())
+    Ok(serialized_metrics)
+}
+
+
+fn createDDataMessages(ddata_file_path: String, numMessages: u32) -> Result<Vec<String>, Box<dyn std::error::Error>> {
+    // Open the file
+    let file = File::open(ddata_file_path)?;
+    let mut reader = BufReader::new(file);
+
+    let mut contents = String::new();
+    reader.read_to_string(&mut contents)?;
+
+    // pass the json
+    let jsonFromFile: Value = serde_json::from_str(&contents)?;
+
+    let metrics = &jsonFromFile["metrics"];
+
+    let mut serialized_metrics = Vec::new();
+    if let serde_json::Value::Array(metrics) = metrics {
+        for metric in metrics {
+          let serialized_metric = serde_json::to_string(metric).unwrap_or_else(|_| "Invalid JSON".to_string());
+          serialized_metrics.push(serialized_metric);
+        }
+    }
+    // now we create the amount of extra messages with the numMessages variable
+    // using the first metric in the serialized_metrics as a tempalte
+    //
+  
+
+if !serialized_metrics.is_empty() {
+    let mut additional_metrics = Vec::new();
+
+    for _ in 0..numMessages {
+        for metric_str in &serialized_metrics {
+            let mut metric: serde_json::Value = serde_json::from_str(metric_str)?;
+
+            if let Some(value) = metric.get_mut("value") {
+               let random_value = rand::thread_rng().gen_range(-10.0..=150.0);
+*value = serde_json::Value::Number(serde_json::Number::from_f64(random_value).unwrap());
+            }
+            // Update other fields as necessary, like timestamp
+
+            let new_serialized_metric = serde_json::to_string(&metric).unwrap_or_else(|_| "Invalid JSON".to_string());
+            additional_metrics.push(new_serialized_metric);
+        }
+    }
+
+    serialized_metrics.extend(additional_metrics);
+}
+
+// Now `serialized_metrics` contains the original and additional metrics
+
+
+      // print the contents for now
+    // println!("I am the json file contents: {:?}", metrics );
+
+    Ok(serialized_metrics)
 }
--- src/simulation/mqtt_handler.rs
+++ src/simulation/mqtt_handler.rs
@@ -4,10 +4,21 @@ use crate::state::AppState;
 use gtk::TextView;
  use crate::simulation::send_payloads_to_broker::send_payloads_to_broker;  
 use crate::simulation::create_payloads::create_payloads;
+
 pub fn execute_simulation(app_state: &Rc<RefCell<AppState>>, text_view: &TextView) {
-    // first we are going to create the functions to create the payloads
     println!("########### woot im doing osmethign ################");
-    let payloads = create_payloads(app_state);
-    // send payloads to broker
-   // send_payloads_to_broker(&app_state, payloads);
+    let payloads_result = create_payloads(app_state);
+
+    match payloads_result {
+        Ok(payloads) => {
+            println!("payloads to push {:?}", payloads);
+            // If send_payloads_to_broker expects Vec<String>
+            send_payloads_to_broker(&app_state, payloads);
+        }
+        Err(e) => {
+            // Handle the error, e.g., log it or display it
+            println!("Error creating payloads: {}", e);
+        }
+    }
 }
+
